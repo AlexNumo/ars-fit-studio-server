57d78911717ea34190a65b0f32140960a3f9b8ea
Arseniuk Oleksandr <korol1511@gmail.com>
2024-02-16 10:49:48 +0200

    New server, test 3

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
-// const MongoClient = require('mongodb').MongoClient;
-const { PORT, DB_HOST } = process.env;
-// const client = new MongoClient(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv').config();
 
-// ==================================== Start the server =====================================================================================
+const { PORT, DB_HOST} = process.env;
+
 
 mongoose.connect(DB_HOST).then(() => {
-  console.log('Database connection successful');
-  app.listen(PORT || 3000);
+    console.log('Database connection successful');
+    app.listen(PORT);
 }).then(() => {
-  console.log(`Server is on ${PORT || 3000}`);
+    console.log(`Server is on ${PORT}`);
 }).catch((err) => {
-  console.log('ERROR', err);
-  process.exit(1);
+    console.log('ERROR', err);
+    process.exit(1);
 });
